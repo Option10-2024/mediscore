886e35b7a3652504565fb92e196157e48b82625a
Mattias Van Eetvelt <mattias.vaneetvelt@student.uclouvain.be>
2023-10-31 12:58:21 +0100

    debug drugsURLsGetter and new stuff

---
+++ webscraping/dirmaker.py
@@ -0,0 +1,12 @@
+import os
+
+def makeOutputDir():
+    cwd = os.getcwd()
+    
+    if not os.path.exists(cwd+'/outputs'):
+        os.makedirs(cwd+'/outputs')
+        print('outputs folder created')
+    else:
+        print('outputs folder already exists')
+
+makeOutputDir() 
--- webscraping/drugsURLsGetter.py
+++ webscraping/drugsURLsGetter.py
@@ -1,8 +1,7 @@
 #%%
 import requests
 from bs4 import BeautifulSoup
-from scrapy import Selector
-from selenium import webdriver
+import dirmaker
 
 # import python scripts
 import createListURLs
@@ -20,19 +19,19 @@ def getAllDrugsUrls(alpha_lst_URLs):
     # Define the domain name
     root = 'https://janusinfo.se/'
     all_drugs_urls = []
-
+    
     for drugs_letter_i in alpha_lst_URLs:
         # print letter being processed
-        current_letter = drugs_letter_i.split('/')[-1]
+        current_letter = drugs_letter_i.split('=')[1][0]
         print("Letter " + str(current_letter) + " is being processed...")
         
-        # Begin scrapping
+        # Begin URL extraction
         urls_letter_i = []
         result  = requests.get(drugs_letter_i)
         content = result.text
         soup = BeautifulSoup(content, 'lxml')
         alpha_table = soup.find('ol', class_='sv-abc-result sv-defaultlist-noleft')
-
+        
         # Make sure the find() does not return None
         if alpha_table:
             # Extract all the URLs from the given webpage
@@ -44,23 +43,30 @@ def getAllDrugsUrls(alpha_lst_URLs):
                 
             # Add all the links for a given letter to the main list
             all_drugs_urls.append(urls_letter_i)
-        
+            
+        elif alpha_table==None :
+            print("    No 'ol' element with the specified class found!")
+            # Check if there is a drug starting with letter *i*
+            print("    Checking if list of drugs is empty")
+            warning_div = soup.find('div', class_="marker_4ba952c316aa46e49e75c8a2")
+            warning_msg = warning_div.text.strip()
+            print("    " + warning_msg)
+            
         else:
             print("No 'ol' element with the specified class found!")
         
-        
-    print('Got all drug URLs :D')
     return all_drugs_urls
     
-
+# %%
 if __name__ == "__main__":
+    dirmaker.makeOutputDir()
     # Create a list containing all the URL of the webpages 
     # with all the links for drug starting with letter *i*
     lst_alpha_urls = createListURLs.CreateListAlphabeticalURLs()
     all_drugs_urls = getAllDrugsUrls(lst_alpha_urls)
     
     # Write the output as a txt file
-    with open('list_drugs_urls.txt','w') as file:
+    with open('./outputs/list_drugs_urls.txt','w') as file:
         for drugs_letter_i in all_drugs_urls:
             for url_drug_i in drugs_letter_i:
                 file.write(url_drug_i)
@@ -69,7 +75,3 @@ if __name__ == "__main__":
     print('URLs of all drugs correctly written!')
     
     
-        
-
-
-# %%
