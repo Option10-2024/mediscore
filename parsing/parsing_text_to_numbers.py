7b931d7d8bc9cd9801e90d3d0c7facd6f5dbfb15
joachim22 <99319281+joachim22@users.noreply.github.com>
2023-11-21 16:01:12 +0100

    end of selection of parsing words

--- parsing/parsing_text_to_numbers.py
+++ parsing/parsing_text_to_numbers.py
@@ -5,7 +5,7 @@ import pandas as pd
 import os
 
 
-df = pd.read_csv('parsing\janus_parsed.csv', sep = ",")
+df = pd.read_csv('janus_parsed.csv', sep = ",")
 
 # Search a specified string in a specified row and column in a DF
 def finder(data, num_row, num_column, find_word) :
@@ -15,24 +15,30 @@ def finder(data, num_row, num_column, find_word) :
 
 def converter2(data, num_row, num_column,words) :
     done = False
+
+    ra = finder(data, num_row, num_column,"lack of data") #generic cases without data
+    rb = finder(data, num_row,num_column,"No data") 
+    rc = finder(data, num_row,num_column,"no data") 
+    rd = finder(data, num_row,num_column,"not enough data") 
+
+    if ra or rb or rc or rd :
+        data.iloc[num_row,num_column] = "pas de données"  #replace the rows without data
+        done = True
+
     for i in words :
         r = finder(data, num_row, num_column,i[0])   #use finder with the text part of words
         if r :
             data.iloc[num_row,num_column] = i[1]     #if found it, replace by the associated value
             done = True
-    ra = finder(data, num_row, num_column,"lack of data") #generic cases without data
-    rb = finder(data, num_row,num_column,"No data") 
-
-    if ra or rb :
-        data.iloc[num_row,num_column] = "pas de données"  #replace the rows without data
-        done = True
+    
     return done
         
 # define the key words
-words1 = [["very persistent",3],["is potentially persistent",3],["is degraded slowly",3],["slowly degraded",3],["slowly degrades",3],["is not persistent",0]]
+words1 = [["very persistent",3],["is potentially persistent",3],["is degraded slowly",3],["slowly degraded",3],["slowly degrade",3],["is not persistent",0],["is potentially peristent", 3],["degraded in the environment", 3],["is likely non-persistent",0],["not considered to be persistent",0],["is persistent",3]]
 #words1 = [["is potentially persistent",3],["is degraded slowly",3],["slowly degraded",3]]
-words2 =[["Has high potential for bioaccumulation",3],["Has low potential for bioaccumulation",0]]
-words3 =[["Very high toxicity",3],["High toxicity",2],["Moderate toxicity",1],["Low toxicity",0]]
+#words2 =[["Has high potential for bioaccumulation",3],["Has low potential for bioaccumulation",0]]
+words2 =[["has high potential for bioaccumulation",3],["low potential",0],["has high potential to bioaccumulate",3],["has high potential to bioaccumulate",3],["has a high potential to bioaccumulate",3],["high potential for bioconcentration",3],["has the potential to be stored",3],["has the potential to bioaccumulate",3],["a strong bioconcentration can be expected",3],["does not have the potential",0],["has no potential to bioaccumulate",0],["No significant bioaccumulation potential",0]]
+words3 =[["very high toxicity",3],["very high acute toxicity",3],["very high chronic toxicity",3],["high toxicity",2],["high acute toxicity",2],["high chronic toxicity",2],["moderate toxicity",1],["moderate chronic toxicity",1],["moderate acute toxicity",1],["low toxicity",0],["low chronic toxicity",0],["low acute toxicity",0],["moderat chronic toxicity",1],["very high acute/chronic toxicity",3],["very hig acute toxicity",3]]
 
 #Applied on the three main columns
 Unmatched = 0
@@ -54,7 +60,8 @@ for i, row in df.iterrows() :
 print(str(Unmatched) + " rows unmatched")
 
 
-df.to_csv('parsing\janus_to_numbers.csv',index=False)
+df.to_csv('janus_to_numbers.csv',index=False)# %%
+
+
+
 # %%
-
-
